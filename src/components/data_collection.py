b4a1b32dfae2a94c2490fde04a52c33ce1affe93
Javier De la Ossa <javdelfer@gmail.com>
2023-05-23 00:02:51 +0200

    Update data collection script with exception handling to avoid halting the execution when 404 error occurs

--- src/components/data_collection.py
+++ src/components/data_collection.py
@@ -1,5 +1,6 @@
 import os
 import urllib.request
+from src.logger import logging
 
 
 i2m = list(zip(range(1, 13), ['Gener', 'Febrer', 'Marc', 'Abril', 'Maig', 'Juny', 'Juliol', 'Agost', 'Setembre', 'Octubre', 'Novembre', 'Desembre']))
@@ -10,12 +11,21 @@ for year in [2022, 2021, 2020, 2019]:
         file_name = f'data/raw/{year}_{month:02d}_{month_name}_BicingNou_ESTACIONS.7z'
         extraction_path = 'data/raw'
         
-        # Download the file using urllib
-        urllib.request.urlretrieve(file_url, file_name)
+        try:
+            # Download the file using urllib
+            urllib.request.urlretrieve(file_url, file_name)
         
+        except urllib.error.HTTPError as e:
+            
+            if e.code == 404:
+                logging.info(f'File not found: {year}_{month:02d}_{month_name}_BicingNou_ESTACIONS.7z')
+                continue
+            
         # Extract the contents of the 7z archive using 7-Zip
         extract_cmd = f'7z x {file_name} -o{extraction_path}'
         os.system(extract_cmd)
         
-        # Remove the downloaded 7z archive file using shutil
+        # Remove the downloaded 7z archive file
         os.remove(file_name)
+        
+        logging.info(f'File successfully downloaded!: {year}_{month:02d}_{month_name}_BicingNou_ESTACIONS.7z')
