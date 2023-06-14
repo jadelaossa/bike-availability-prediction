6aab855513fa579c5ddfe14992997a8d34c11b7f
Javier De la Ossa <javdelfer@gmail.com>
2023-06-14 22:39:31 +0200

    Save feature transformation as .pkl object and create prediction pipeline

--- src/pipeline/make_prediction.py
+++ src/pipeline/make_prediction.py
@@ -1,6 +1,32 @@
-# From the basic features configuration, creates a rich right input for the trained model.
-# Pipeline:
-# 1. tweak_bicing -> merges the data tables
-# 2. Feature transformation (outputs a .pkl file with the test data transformed -> /artifacts)
-# Loads saved model with the best score (model.pkl)
-# Makes prediction and outputs a .csv file with the right format (index + precentage_docks_available) for a kaggle submission -> /artifacts.
+import numpy as np
+import pandas as pd
+from src.utils import load_object, TweakBicingTransformer
+
+import matplotlib.pyplot as plt
+
+
+
+metadata = pd.read_csv(r'src\pipeline\artifacts\metadata_sample_submission.csv', index_col='index').rename(columns={'ctx-1': 'ctx_1', 'ctx-2': 'ctx_2',
+                                                                                                         'ctx-3': 'ctx_3', 'ctx-4': 'ctx_4'})
+
+bicing_info = pd.read_parquet(r'data\processed\bicing_info.parquet')
+meteo_stats = pd.read_parquet(r'data\processed\meteo_stats.parquet').query('year == 2023').drop(columns='year')
+calendar = pd.read_parquet(r'data\processed\calendar.parquet').query('year == 2023').drop(columns='year')
+    
+bicing = (metadata
+          .merge(bicing_info, on=['station_id'], how='left', validate='many_to_one')
+          .merge(calendar, on=['month', 'day'], how='left', validate='many_to_one')
+          .merge(meteo_stats, on=['month', 'day'], how='left', validate='many_to_one')
+)
+
+bicing_pl = load_object(r'models\features_transformation_pipeline.pkl')
+X_sample = bicing_pl.transform(bicing)
+
+model = load_object(r'models\model.pkl')
+
+y_pred = model.predict(X_sample)
+
+metadata_pred = (pd.DataFrame(y_pred)
+                 .reset_index()
+                 .rename(columns={0: 'percentage_docks_available'})
+).to_csv(r'src\pipeline\artifacts\metadata_pred.csv', index=False)
--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,7 @@
 import os
 import pickle
+import pandas as pd
+from sklearn import base
 
 
 def save_object(file_path, object):
@@ -12,3 +14,31 @@ def save_object(file_path, object):
 def load_object(file_path):
     with open(file_path, 'rb') as f:
         return pickle.load(f)
+    
+def tweak_bicing(df_):
+    
+    columns_to_drop = ['post_code', 'lat', 'lon', 'capacity', 'max_temp', 'min_temp', 'avg_atm_pressure', 
+                       'avg_wind_direction', 'max_wind_speed', 'max_streak_wind_direction']
+
+    return (df_
+            .astype({'station_id': 'category', 'post_code': 'category'})  # to be treated as categorical variable by algorithms like XGBoost
+            .drop(columns=columns_to_drop)
+            )
+
+class TweakBicingTransformer(base.BaseEstimator, base.TransformerMixin):
+    def __init__(self, ycol=None):
+        self.ycol = ycol
+        
+    def transform(self, X):
+        return tweak_bicing(X)
+    
+    def fit(self, X, y=None):
+        return self
+    
+def get_rawX_y(df, y_col, drop_set):
+    raw = (df
+           .set_index('year')
+           .drop(index=drop_set)
+           .reset_index()
+          )
+    return raw.drop(columns=['year', y_col]), raw[y_col]
